d180ed739f62cea67e5e398cfff646733cb8a6dc
aleriiu <aleriiiu@gmail.com>
2023-10-11 18:14:06 +0300

    small changes

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ form.addEventListener('submit', async (e) => {
     const nameInput = form.querySelector('.form__input-name');
     const phoneInput = form.querySelector('.form__input-numb');
     const messageInput = form.querySelector('.form__input-message');
-    const url = 'https://api.telegram.org/bot6572409290:AAFo8jBULV5LTRvI5NOhEQYIz5ZaQsVHD98/sendMessage';
+    const url = '#';
 
     const text = `Сообщение из формы\r\n\r\nИмя: ${nameInput.value}\r\nТелефон: ${phoneInput.value}\r\nСообщение: ${messageInput.value}`;
 
